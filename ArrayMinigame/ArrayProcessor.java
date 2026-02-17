a92ec90baf665ee0fd3c3648ebf39cbd8f369709
JourneyToMasteryFromContributor <dburnsidework@gmail.com>
2026-02-17 20:06:43 +0000

    Add files via upload

---
+++ ArrayMinigame/ArrayProcessor.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class ArrayProcessor {
+    private double dividerInput;
+    private int indexInput;
+    private int[] numbers = new int[10];
+
+    public ArrayProcessor(double dividerInput, int indexInput) {
+        this.dividerInput = dividerInput;
+        this.indexInput = indexInput - 1;
+    }
+
+    public void randomNumArray10() {
+        Random randomNum = new Random();
+        for (int i = 0; i < numbers.length; i++) {
+            this.numbers[i] = randomNum.nextInt(100);
+        }
+    }
+
+    public int getRanNumFromArray() {
+        return this.numbers[this.indexInput];
+    }
+
+    public double processArray() {
+        return this.numbers[this.indexInput] / this.dividerInput;
+    }
+}
---
+++ ArrayMinigame/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Please enter an index including between 1 and 10: ");
+        int indexInput = scan.nextInt();
+
+        System.out.print("Please enter a divider: ");
+        double dividerInput = scan.nextDouble();
+
+        ArrayProcessor array1 = new ArrayProcessor(dividerInput, indexInput);
+        array1.randomNumArray10();
+
+        try {
+            System.out.println("Number: " + array1.getRanNumFromArray() + " Divided by " + dividerInput + " = " + array1.processArray());
+        } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Outside of array.");
+        } catch (ArithmeticException e) { // Included although doubles can handle it, I just want to add it for future reference for int.
+            System.out.println("Cannot divide by 0");
+        }
+        finally {
+            System.out.println("Operation complete.");
+        }
+
+    }
+}
